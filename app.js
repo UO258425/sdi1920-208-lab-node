4c4b40ed485872ba91cea514802a0f169a2017c7
Carlos Manrique Enguita <UO258425@uniovi.es>
2020-03-15 21:12:32 +0100

    SDI-NodeJS-8,9,10 Recursos estáticos,Peticiones POST y parámetros, Enrutamiento y comodines

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 //Módulos
 let express = require('express');
 let app = express();
+let bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static('public'));
 
 app.set('port', 8081);
 
-//Rutas/controladoresporlógica
 require("./routes/rusuarios.js")(app);
 require("./routes/rcanciones.js")(app);
 
-app.listen(app.get('port'), function(){
+app.listen(app.get('port'), function () {
     console.log("Servidor activo");
 });
