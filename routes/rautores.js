48a6a891ef09943c1c24cdf2a71c179a644d57b0
Carlos Manrique Enguita <UO258425@uniovi.es>
2020-03-19 13:19:12 +0100

    SDI–session7-Ejercicio-complementario1

---
+++ routes/rautores.js
@@ -0,0 +1,50 @@
+module.exports = function (app, swig) {
+
+    app.get("/autores", function (req, res) {
+        let autores = [{
+            "nombre": "Lars Ulrich",
+            "grupo": "Metallica",
+            "rol":"batería"
+        }, {
+            "nombre": "Eddie Van Halen",
+            "grupo": "Van Halen",
+            "rol":"Guitarrista"
+        },{
+            "nombre": "Fito Cabrales",
+            "grupo": "Platero y tu",
+            "rol":"Cantante"
+        }];
+
+        let respuesta = swig.renderFile("views/autores.html", {
+            autores: autores
+        });
+
+        res.send(respuesta);
+    });
+
+    app.get("/autores/agregar", function(req, res) {
+        let respuesta = swig.renderFile("views/autores-agregar.html", {});
+        res.send(respuesta);
+    });
+
+    app.post("/autores/agregar", function(req, res) {
+        let errorResponse = "";
+        if (req.body.nombre === null || req.body.nombre === "" || typeof req.body.nombre === undefined)
+            errorResponse+="Nombre no enviado en la petición";
+
+        if (req.body.grupo === null || req.body.grupo === "" || typeof req.body.grupo === undefined)
+            errorResponse+="<br>Grupo  no enviado en la petición";
+
+        if (req.body.rol === null || req.body.rol === "" || typeof req.body.rol === undefined)
+            errorResponse+="<br>Rol no enviado en la petición";
+
+        if(errorResponse=="")
+            res.send("Autor agregado: " + req.body.nombre + "<br>" + " Grupo: " + req.body.grupo + "<br>" + " Rol: " + req.body.rol);
+        else
+            res.send(errorResponse);
+    });
+
+    app.get('/autores/*', function(req, res) {
+        res.redirect("/autores");
+    });
+};
