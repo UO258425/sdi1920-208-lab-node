c46e99bc39835e9ff0b58497dbcf5bba4e21c878
Carlos Manrique Enguita <UO258425@uniovi.es>
2020-03-25 15:17:12 +0100

    “SDINodeJS-8.9 y 8.10 - Colecciones relacionadas y routers

--- app.js
+++ app.js
@@ -15,24 +15,56 @@ let fileUpload = require('express-fileupload');
 app.use(fileUpload());
 
 let mongo = require('mongodb');
-let swig  = require('swig');
+let swig = require('swig');
 
 let bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
-
-
 let gestorBD = require("./modules/gestorDB.js");
-gestorBD.init(app,mongo);
+gestorBD.init(app, mongo);
+
+// routerUsuarioSession
+let routerUsuarioSession = express.Router();
+routerUsuarioSession.use(function (req, res, next) {
+    console.log("routerUsuarioSession");
+    if (req.session.usuario) {
+        // dejamos correr la petición
+        next();
+    } else {
+        console.log("va a : " + req.session.destino)
+        res.redirect("/identificarse");
+    }
+});
+//Aplicar routerUsuarioSession
+app.use("/canciones/agregar", routerUsuarioSession);
+app.use("/publicaciones", routerUsuarioSession);
+
+//routerAudios
+let routerAudios = express.Router();
+routerAudios.use(function (req, res, next) {
+    console.log("routerAudios");
+    let path = require('path');
+    let idCancion = path.basename(req.originalUrl, '.mp3');
+    gestorBD.obtenerCanciones(
+        {"_id": mongo.ObjectID(idCancion)}, function (canciones) {
+            if (req.session.usuario && canciones[0].autor == req.session.usuario) {
+                next();
+            } else {
+                res.redirect("/tienda");
+            }
+        })
+});
+//Aplicar routerAudios
+app.use("/audios/", routerAudios);
 
 
 app.use(express.static('public'));
 
 app.set('port', 8081);
-app.set('db','mongodb://tiendamusicaadmin:7UsGPXKsQxHD1xO5@tiendamusica-shard-00-00-uchuk.mongodb.net:27017,tiendamusica-shard-00-01-uchuk.mongodb.net:27017,tiendamusica-shard-00-02-uchuk.mongodb.net:27017/test?ssl=true&replicaSet=tiendamusica-shard-0&authSource=admin&retryWrites=true');
-app.set('clave','abcdefg');
-app.set('crypto',crypto);
+app.set('db', 'mongodb://tiendamusicaadmin:7UsGPXKsQxHD1xO5@tiendamusica-shard-00-00-uchuk.mongodb.net:27017,tiendamusica-shard-00-01-uchuk.mongodb.net:27017,tiendamusica-shard-00-02-uchuk.mongodb.net:27017/test?ssl=true&replicaSet=tiendamusica-shard-0&authSource=admin&retryWrites=true');
+app.set('clave', 'abcdefg');
+app.set('crypto', crypto);
 
 require("./routes/rusuarios.js")(app, swig, gestorBD);
 require("./routes/rcanciones.js")(app, swig, gestorBD);
